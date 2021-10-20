5f957aaa0f1e7ea8291d8e0e7b935617493f95de
yunuserogluer <76969303+yunuserogluer@users.noreply.github.com>
2021-10-20 23:26:26 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,52 @@
+print("""
+*********************************
+* MAL CEMRE İÇİN HESAP MAKİNESİ *
+*********************************
+""")
+
+sayi1 = int(input("İlk sayıyı gir: "))
+sayi2 = int(input("İkinci sayıyı gir: "))
+sor =   input("""Hangi işlemi yapacan MAL CEMRE?
+A = Toplama
+B = Çıkarma
+C = Bölme
+D = Çarpma
+""")
+toplam = sayi1+sayi2
+cıkarma = sayi1-sayi2
+bol = sayi1/sayi2
+carp = sayi1*sayi2
+
+if sor == "A":
+    if toplam == 31:
+        print("FSAFPAFKPOAEFKOPKSFSA ;)")
+
+    else:
+        print(toplam)
+
+if sor == "B":
+    if cıkarma == 31:
+        print("FSAFPAFKPOAEFKOPKSFSA ;)")
+
+    else:
+        print(cıkarma)
+
+if sor == "C":
+    if bol == 31:
+        print("FSAFPAFKPOAEFKOPKSFSA ;)")
+
+    else:
+        print(bol)
+
+if sor == "D":
+    if carp == 31:
+        print("FSAFPAFKPOAEFKOPKSFSA ;)")
+
+    else:
+        print(carp)
+
+
+
+
+
+
